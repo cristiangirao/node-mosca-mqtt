bc40f1d4a61ea9817c60411146eeeed797d5e18d
Cristian Girão <cristiangirao97@gmail.com>
2019-02-05 23:01:45 -0200

    feature-- Adição de um exemplo de cliente externo e exibição de mensagens que não são do formato JSON.

--- mqtt-server.js
+++ mqtt-server.js
@@ -2,7 +2,7 @@ const mosca = require('mosca');
 var server = new mosca.Server({ port: 1884} );
 
 server.on('clientConnected', function(client) {
-    console.log('Cliente conectado', client.id);
+    console.log('Cliente conectado:', client.id);
 });
 
 //Evento disparado quando um cliente envia uma mensagem
@@ -10,11 +10,13 @@ server.on('published', async function(packet) {
   try{
     if( JSON.parse(packet.payload.toString('utf8')).hasOwnProperty('produto') ){
       var obj = await JSON.parse(packet.payload.toString('utf8'));
-      console.log('Produto: '+obj.produto);
-      console.log('Quantidade: '+obj.quantidade);
+      console.log('Produto: ',obj.produto);
+      console.log('Quantidade: ',obj.quantidade);
+    }else{
+    	console.log('Mensagem recebida:',packet.payload.toString('utf8'));
     }
   }catch(e){
-    
+    console.log(packet.payload.toString());
   }
 });
 
